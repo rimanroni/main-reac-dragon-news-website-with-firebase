b3d8ef25932cb97a544b66caac52e02c61a3d1e7
riman ronie <rimanroni386@gmail.comm>
2024-03-03 20:34:06 +0600

    all config setup done now start project

---
+++ src/Firebase/FirebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAmsNeEPis-TYk0a6muSSsaF1sAeasEkWU",
+  authDomain: "main-dragon-website.firebaseapp.com",
+  projectId: "main-dragon-website",
+  storageBucket: "main-dragon-website.appspot.com",
+  messagingSenderId: "5977897181",
+  appId: "1:5977897181:web:8a8b492baa93ce2f494334"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
